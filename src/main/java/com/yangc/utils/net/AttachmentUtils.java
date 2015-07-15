d5a7951da422a407bc637489d19b427db7a3aa61
yangchenjava <yangchen_java@126.com>
2015-07-15 23:17:46 +0800

    AttachmentUtils

--- src/main/java/com/yangc/utils/net/AttachmentUtils.java
+++ src/main/java/com/yangc/utils/net/AttachmentUtils.java
@@ -15,35 +15,37 @@ public class AttachmentUtils {
 	}
 
 	public static void convertAttachmentFileName(HttpServletRequest request, HttpServletResponse response, String fileName) {
-		try {
-			fileName = URLEncoder.encode(fileName, "UTF-8");
-			String userAgent = request.getHeader("User-Agent");
-			if (StringUtils.isNotBlank(userAgent)) {
-				userAgent = userAgent.toLowerCase();
-				// IE浏览器只能采用URLEncoder编码
-				if (userAgent.indexOf("msie") != -1) {
-					fileName = "filename=\"" + fileName + "\"";
+		if (StringUtils.isNotBlank(fileName)) {
+			try {
+				fileName = URLEncoder.encode(fileName, "UTF-8").replaceAll("\\+", "%20");
+				String userAgent = request.getHeader("User-Agent");
+				if (StringUtils.isNotBlank(userAgent)) {
+					userAgent = userAgent.toLowerCase();
+					// IE浏览器只能采用URLEncoder编码
+					if (userAgent.indexOf("msie") != -1) {
+						fileName = "filename=\"" + fileName + "\"";
+					}
+					// Opera浏览器只能采用filename*
+					else if (userAgent.indexOf("opera") != -1) {
+						fileName = "filename*=UTF-8''" + fileName;
+					}
+					// Chrome浏览器只能采用MimeUtility编码或ISO编码的中文输出
+					else if (userAgent.indexOf("chrome") != -1) {
+						fileName = "filename=\"" + MimeUtility.encodeText(fileName, "UTF-8", "B") + "\"";
+					}
+					// Safari浏览器只能采用ISO编码的中文输出
+					else if (userAgent.indexOf("safari") != -1) {
+						fileName = "filename=\"" + new String(fileName.getBytes("UTF-8"), "ISO-8859-1") + "\"";
+					}
+					// FireFox浏览器可以使用MimeUtility或filename*或ISO编码的中文输出
+					else if (userAgent.indexOf("mozilla") != -1) {
+						fileName = "filename*=UTF-8''" + fileName;
+					}
 				}
-				// Opera浏览器只能采用filename*
-				else if (userAgent.indexOf("opera") != -1) {
-					fileName = "filename*=UTF-8''" + fileName;
-				}
-				// Safari浏览器只能采用ISO编码的中文输出
-				else if (userAgent.indexOf("safari") != -1) {
-					fileName = "filename=\"" + new String(fileName.getBytes("UTF-8"), "ISO8859-1") + "\"";
-				}
-				// Chrome浏览器只能采用MimeUtility编码或ISO编码的中文输出
-				else if (userAgent.indexOf("applewebkit") != -1) {
-					fileName = "filename=\"" + MimeUtility.encodeText(fileName, "UTF-8", "B") + "\"";
-				}
-				// FireFox浏览器可以使用MimeUtility或filename*或ISO编码的中文输出
-				else if (userAgent.indexOf("mozilla") != -1) {
-					fileName = "filename*=UTF-8''" + fileName;
-				}
+				response.setHeader("Content-Disposition", "attachment; " + fileName);
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
 			}
-			response.setHeader("Content-Disposition", "attachment; " + fileName);
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
 		}
 	}
 
