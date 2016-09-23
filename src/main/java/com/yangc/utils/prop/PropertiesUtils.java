76e2f2b48b7e31548d6004ef5f58e565ae14ac4e
yangchenjava <yangchen_java@126.com>
2016-09-23 23:43:56 +0800

    fix bug

--- src/main/java/com/yangc/utils/prop/PropertiesUtils.java
+++ src/main/java/com/yangc/utils/prop/PropertiesUtils.java
@@ -1,21 +1,12 @@
 package com.yangc.utils.prop;
 
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.InputStream;
 import java.util.Properties;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 import org.apache.commons.lang3.StringUtils;
 
 public class PropertiesUtils {
-
-	private static final String UTF_8 = "UTF-8";
-
-	private static final ConcurrentMap<String, Properties> PROPS = new ConcurrentHashMap<String, Properties>();
 
 	private PropertiesUtils() {
 	}
@@ -32,20 +23,7 @@ public class PropertiesUtils {
 		if (StringUtils.isBlank(filePath) || StringUtils.isBlank(name)) {
 			throw new IllegalArgumentException("The parameters must not be null");
 		}
-		try {
-			Properties prop = PROPS.get(filePath);
-			if (prop == null) {
-				prop = new Properties();
-				prop.load(PropertiesUtils.class.getResourceAsStream(filePath));
-				PROPS.put(filePath, prop);
-			}
-			return prop.getProperty(name);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
+		return getProperty(filePath, name, null);
 	}
 
 	/**
@@ -58,71 +36,28 @@ public class PropertiesUtils {
 	 * @return
 	 */
 	public static String getProperty(String filePath, String name, String defaultValue) {
-		if (StringUtils.isBlank(filePath) || StringUtils.isBlank(name) || StringUtils.isBlank(defaultValue)) {
+		if (StringUtils.isBlank(filePath) || StringUtils.isBlank(name)) {
 			throw new IllegalArgumentException("The parameters must not be null");
 		}
+		Properties prop = new Properties();
+		InputStream in = null;
 		try {
-			Properties prop = PROPS.get(filePath);
-			if (prop == null) {
-				prop = new Properties();
-				prop.load(PropertiesUtils.class.getResourceAsStream(filePath));
-				PROPS.put(filePath, prop);
-			}
-			String value = prop.getProperty(name);
-			return StringUtils.isBlank(value) ? defaultValue : value;
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return defaultValue;
-	}
-
-	/**
-	 * @功能: properties文件中的name,value (已过期, 会打乱properties文件中顺序, 其中加锁是为了防止多个线程同时写文件)
-	 * @作者: yangc
-	 * @创建日期: 2013-11-21 下午07:02:38
-	 * @param filePath properties文件路径(classpath中的相对路径)
-	 * @param name
-	 * @param value
-	 */
-	@Deprecated
-	public static void setProperty(String filePath, String name, String value) {
-		if (StringUtils.isBlank(filePath) || StringUtils.isBlank(name) || StringUtils.isBlank(value)) {
-			throw new IllegalArgumentException("The parameters must not be null");
-		}
-		BufferedWriter bw = null;
-		try {
-			synchronized (PROPS) {
-				Properties prop = PROPS.get(filePath);
-				if (prop == null) {
-					prop = new Properties();
-					prop.load(PropertiesUtils.class.getResourceAsStream(filePath));
-					PROPS.put(filePath, prop);
-				}
-				prop.put(name, value);
-				bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(PropertiesUtils.class.getResource(filePath).getFile()), UTF_8));
-				prop.store(bw, "保存properties配置文件");
-			}
-			bw.flush();
-			bw.close();
-			bw = null;
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			in = PropertiesUtils.class.getResourceAsStream(filePath);
+			prop.load(in);
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
 			try {
-				if (bw != null) bw.close();
+				if (in != null) in.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
+		return prop.getProperty(name, defaultValue);
 	}
 
 	public static void main(String[] args) {
 		System.out.println(PropertiesUtils.getProperty("/jdbc.properties", "oracle.driver"));
-		PropertiesUtils.setProperty("/email.properties", "name", "yangc");
 	}
 
 }
